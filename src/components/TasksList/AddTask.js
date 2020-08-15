599edc0e68e575e1ffc2c85a7a4d344bdfe97f02
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-08-15 18:36:25 +0200

    Add task component structure. Handle task text change

--- src/components/TasksList/AddTask.js
+++ src/components/TasksList/AddTask.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddTask extends Component {
-    state = {}
-    render() {
-        return (
-            <div>
-                Add new Tasks form
-            </div>
-        )
-    };
+  state = {
+    text: "",
+    isPriority: false,
+  };
+
+  handleTextChange = (e) => {
+    const text = e.target.value;
+    this.setState({
+      text,
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Add new task"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <label htmlFor="prior">
+          <input type="checkbox" id="prior" value={this.state.isPriority} />
+          Priority
+        </label>
+        <br />
+        <label htmlFor="date">
+          Deadline
+          <input
+            type="date"
+            id="date"
+            value="2020-08-15"
+            min="2020-08-15"
+            max="2021-01-01"
+          />
+        </label>
+        <br />
+        <button type="submit">ADD</button>
+      </form>
+    );
+  }
 }
 
 export default AddTask;
